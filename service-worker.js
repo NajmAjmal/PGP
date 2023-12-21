6e1831962683a77f174ce2a016302b1aa1c1aa43
Najm Ajmal <151127614+NajmAjmal@users.noreply.github.com>
2023-12-21 14:08:56 +0000

    Service worker simplified

--- service-worker.js
+++ service-worker.js
@@ -1,73 +1,34 @@
-const CACHE_NAME = 'PGP-0-0-2';
+const cacheName = 'pgpToolCache';
+const otherResources = [
+    '/',
+    '/index.html',
+    '/favicon.ico',
+    '/najmajmal',
+    '/service-worker.js',
+    '/assets/css/bootstrap.css',
+    '/assets/css/custom.css',
+    '/assets/fonts/glyphicons-halflings-regular.woff2',
+    '/assets/fonts/key_24x24.png',
+    '/assets/js/bootstrap.min.js',
+    '/assets/js/FileSaver.js',
+    '/assets/js/jquery-3.2.1.min.js',
+    '/assets/js/kbpgp.js',
+    '/assets/js/pgpkeygen.js',
+    '/assets/js/script.js',
+];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            // Cache other essential resources
-            const otherResources = [
-                '/',
-                '/index.html',
-                '/favicon.ico',
-                '/najmajmal',
-                '/service-worker.js',
-                '/assets/css/bootstrap.css',
-                '/assets/css/custom.css',
-                '/assets/fonts/glyphicons-halflings-regular.woff2',
-                '/assets/fonts/key_24x24.png',
-                '/assets/js/bootstrap.min.js',
-                '/assets/js/FileSaver.js',
-                '/assets/js/jquery-3.2.1.min.js',
-                '/assets/js/kbpgp.js',
-                '/assets/js/pgpkeygen.js',
-                '/assets/js/script.js',
-            ];                    
-
-            const resourceCachePromises = otherResources.map((resource) => {
-                return cache.add(resource);
-            });
-
-            return Promise.all(resourceCachePromises);
+        caches.open(cacheName).then(cache => {
+            return cache.addAll(otherResources.map(url => new URL(url, location.href).toString()));
         })
     );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        // Delete old caches to clean up
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            // If the resource is in cache, return it
-            if (response) {
-                return response;
-            }
-
-            // If the resource is not in cache, fetch and cache it
-            return fetch(event.request).then((response) => {
-                // Clone the response to cache and return it
-                const responseToCache = response.clone();
-
-                caches.open(CACHE_NAME).then((cache) => {
-                    cache.put(event.request, responseToCache);
-                });
-
-                return response;
-            }).catch(() => {
-                // If fetch fails, return a fallback response
-                return caches.match('/index.html');
-            });
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
